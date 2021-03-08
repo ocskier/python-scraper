cb8647ddba8b5174c842c3cf76544d5615206516
Jon Jackson <ocskier@gmail.com>
2021-03-08 10:16:09 -0500

    feat: initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# bokeh line graph
+
+# imports
+import bokeh
+from bokeh.plotting import figure, output_notebook, show
+
+output_notebook()
+
+p = figure(plot_width=400,plot_height=400)
+
+show(p)
