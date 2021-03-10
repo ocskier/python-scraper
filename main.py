9330a31e076f5101ac57176c79b84a1eda9f5892
Jon Jackson <ocskier@gmail.com>
2021-03-10 00:46:08 -0500

    feat: add scrape local real estate

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # imports
 import bokeh
+from bs4 import BeautifulSoup
 import requests
 from requests.exceptions import RequestException
 # from bokeh.plotting import figure, output_notebook, show
 
 try:
-    url = "https://www.npr.org/sections/music-news/"
+    url = "https://www.century21.com/real-estate/raleigh-nc-27613/LZ27613/"
     response = requests.get(url)
     html = response.text
-    print(html)
+    soup = BeautifulSoup(html, features="html.parser")
+    for card in soup.find_all('div', 'infinite-item', 'property-card'):
+        print(card.prettify())
+
 except RequestException as e:
     print(e)
 
