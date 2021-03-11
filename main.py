c79887b76d63b5443aef7155e0b6c2b8a08b4168
Jon Jackson <ocskier@gmail.com>
2021-03-11 00:12:29 -0500

    feat: add beds,baths,address,working links to sheet

--- main.py
+++ main.py
@@ -21,22 +21,51 @@ if html.strip():
     soup = BeautifulSoup(html, features="html.parser")
     prices = []
     pending = []
+    beds = []
+    baths = []
+    address = []
+    links = []
     for card in soup.find_all('div', {'class': ['infinite-item', 'property-card']}):
-        if card.find('a') is not None:
-            sale_price = card.find('a').contents
+        if card.find('div',{'class':'property-address'}) is not None:
+            # add price
+            sale_price = card.find('a').contents[0]
             prices.append(int(
-                sale_price[0].replace(',','').replace("'", '').replace('$', '').strip())
+                sale_price.replace(',','').replace("'", '').replace('$', '').strip())
             )
+            # add if avail
             if card.find('div', {'class':'sale-pending'}) is not None:
-                sale_pending = card.find('div', {'class':'sale-pending'}).contents
-                print(sale_pending[0])
+                sale_pending = card.find('div', {'class':'sale-pending'}).contents[0]
                 pending.append(True)
             else:
                 pending.append(False)
+            # of beds
+            num_beds = card.find('div', {'class':'property-beds'}).contents[1].contents[0]
+            beds.append(int(num_beds))
+            # of baths
+            num_baths = card.find('div', {'class':'property-baths'}).contents[1].contents[0]
+            baths.append(int(num_baths))
+            # add address
+            where = card.find('div',{'class':'property-address'}).contents[0].strip()
+            where += ', ' + card.find('div',{'class':'property-city'}).contents[0].strip()
+            address.append(where)
+            # add website link
+            link = 'https://www.century21.com' + card.find('a')['href']
+            links.append(link.replace("'", ''))
 
-print(prices,pending)
-df = pandas.DataFrame(data={"Sale Price": prices,"Sale Pending":pending})
+xl_links = []
+for each in links:
+    xl_links.append('=hyperlink("{url}","Go")'.format(url=each))
+
+# output to csv and excel
+df = pandas.DataFrame(data={
+    "Sale Price": prices,
+    "Sale Pending":pending,
+    "Beds": beds,
+    "Baths": baths,
+    "Address":address,
+    "Url": links})
 df.to_csv("./file.csv", sep=',',index=False)
+df["Url"] = xl_links
 df.to_excel("./sheet.xlsx",index=False)
 
 # output_notebook()
